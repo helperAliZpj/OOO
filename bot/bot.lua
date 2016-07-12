7591bf1f41bd0e33b79c272ed8b15f153d8d672e
helperAliZpj <xychetboyyx@gmail.com>
2016-07-11 18:00:45 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -259,7 +259,7 @@ function create_config()
     globally_banned = {},
     mkgroup = {founded = '', founder = '', title = '', gtype = '', uid = ''},
     realm = {},
-    sudo_users = {[our_id] = our_id}
+    sudo_users = {211303414},--Sudo users
   }
   save_config()
 end
